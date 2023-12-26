06409c11fbdcbed284d5cadb5ce2ca47dd42ced9
Arthur Dercq <arthur.dercq@skynet.be>
2023-12-26 11:55:01 -0400

    put palindrome function to production

--- bundle.js
+++ bundle.js
@@ -1,3 +1,19 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("arthur_dercq_palindrome");
+
+let string = prompt("Please enter a word for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
+
+},{"arthur_dercq_palindrome":2}],2:[function(require,module,exports){
+module.exports = Phrase;
+
+
 // Reverses a string.
 function reverse(string) {
   // return string.split("").reverse().join("");
@@ -23,9 +39,13 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+  }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
@@ -60,3 +80,5 @@ TranslatedPhrase.prototype = new Phrase();
 
 let frase = new TranslatedPhrase("recognize", "reconocer");
 console.log(frase.palindrome());
+
+},{}]},{},[1]);
